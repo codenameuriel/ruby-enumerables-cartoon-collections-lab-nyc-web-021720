41ec8fb973b20b180ddcff8be6fa636d87e7693f
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-02-06 22:38:34 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,20 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+    roll_call = array.each_with_index { |element, index| puts "#{index + 1}. #{element}" }
+    return roll_call
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map { |element| "#{element.capitalize}!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? { |element| element.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if array.find { |element| cheese_types.include?(element) }
+      return array.find { |element| cheese_types.include?(element) }
+  end
+  nil
 end
