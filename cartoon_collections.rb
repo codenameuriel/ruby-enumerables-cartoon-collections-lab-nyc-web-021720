c6cfad7f83ade003a8129bee6f801979963ad8b8
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-02-06 22:35:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def long_planeteer_calls(array)
   array.any? { |element| element.length > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   if array.find { |element| cheese_types.include?(element) }
       return array.find { |element| cheese_types.include?(element) }
