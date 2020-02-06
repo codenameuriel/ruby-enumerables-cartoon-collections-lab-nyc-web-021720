83cbee63863797444c78912d49cb74bd467dbaac
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-02-06 20:21:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,14 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+    roll_call = array.each_with_index { |element, index| puts "#{index + 1}. #{element}" }
+    return roll_call
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map { |element| "#{element.capitalize}!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? { |element| element.length > 4 }
 end
 
 def find_the_cheese# code an argument here
